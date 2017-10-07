038b2097dcae913896be32906d19aa48feb996b9
Lentosy <bertdesaffel@gmail.com>
2017-10-07 11:39:41 +0200

    C reeks 2

--- Semester 1/Programmeren in C en C++/LABO/oefeningen/Reeks 2/15.c
+++ Semester 1/Programmeren in C en C++/LABO/oefeningen/Reeks 2/15.c
@@ -5,16 +5,31 @@
 #include <stdbool.h>
 #include <time.h>
 
-
-
 #define MIN 100
 #define MAX 120
-#define COUNT 20
+#define COUNT (MAX - MIN) + 1
 
 int main(){
 
-	
+	srand(time(NULL));
 
+	bool numbers[COUNT] = { false };
+
+	int i;
+	for(i = 0; i < COUNT; i++){
+		int r = (int)rand() % (MAX - MIN + 1) + MIN;
+		numbers[r - MIN] = true;
+		printf("%d\n", r);
+	}
+
+	printf("-----\n");
+
+	for(i = 0; i < COUNT; i++){
+		if(numbers[i] == false){
+			printf("%d\n", i + MIN);
+		}
+	}
+	
 }
 
 
