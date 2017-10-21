ce5fa96b102b938bd1351e9148862b6716f148cb
Bert De Saffel <bertdesaffel@gmail.com>
2017-10-21 15:31:37 +0200

    android

--- Semester 1/Ontwerp en ontwikkeling van mobiele toepassingen/LABO/QuizApp/app/src/main/java/bert/quizapp/MainActivity.java
+++ Semester 1/Ontwerp en ontwikkeling van mobiele toepassingen/LABO/QuizApp/app/src/main/java/bert/quizapp/MainActivity.java
@@ -4,8 +4,10 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,9 +18,12 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     TextView textView_question;
+    EditText editText_answer;
     Button button_hint;
 
     String[] questions;
+    String[] answers;
+
     int currentQuestionIndex = 0;
     boolean hasRequestedHint;
 
@@ -29,9 +34,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         questions = getResources().getStringArray(R.array.questions);
+        answers = getResources().getStringArray(R.array.answers);
 
         textView_question = (TextView) findViewById(R.id.textView_question);
-        textView_question.setText(getRandomQuestion());
+
+        editText_answer = (EditText) findViewById(R.id.editText_answer);
 
         button_hint = (Button) findViewById(R.id.button_hint);
         button_hint.setOnClickListener(new View.OnClickListener() {
@@ -42,23 +49,43 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent, TAGS.HINT_REQUEST_CODE);
             }
         });
+
+        newQuestion();
     }
 
     public void submit(View view) {
-        Toast.makeText(this, "Submitting answer...", Toast.LENGTH_SHORT).show();
+        String message;
+
+        String correctAnswer = answers[currentQuestionIndex].toLowerCase();
+        String givenAnswer = editText_answer.getText().toString().toLowerCase();
+
+        if (givenAnswer.equals(correctAnswer)) {
+            if (hasRequestedHint) {
+                message = getResources().getString(R.string.correct_answer);
+            } else {
+                message = getResources().getString(R.string.correct_answer_with_hint);
+            }
+        } else {
+            message = getResources().getString(R.string.wrong_answer);
+        }
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+
+        newQuestion();
     }
 
-    private String getRandomQuestion(){
+    private void newQuestion() {
         Random r = new Random();
         currentQuestionIndex = r.nextInt(questions.length);
-        return questions[currentQuestionIndex];
+        textView_question.setText(questions[currentQuestionIndex]);
+        hasRequestedHint = false;
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode == TAGS.HINT_REQUEST_CODE){
-            if(resultCode == RESULT_OK){
+        if (requestCode == TAGS.HINT_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
                 hasRequestedHint = data.getBooleanExtra(TAGS.HAS_REQUESTED_HINT, false);
+                Log.d(TAGS.HAS_REQUESTED_HINT, Boolean.toString(hasRequestedHint));
             }
         }
     }
